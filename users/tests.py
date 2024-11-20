5eb63275d0bcbd02e7d2147c02e695d1b996ad40
Victor Neo <icyisamu@gmail.com>
2024-11-20 15:09:50 +1100

    Allow login and signup via Google

--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,11 @@
+import json
+from unittest.mock import patch
+
 import pytest
 from django.urls import reverse
 from pytest_django.asserts import assertRedirects, assertTemplateUsed
 
-from .models import User
+from .models import ExternalProfile, User
 
 
 @pytest.mark.django_db
@@ -44,3 +47,46 @@ def test_logout(client, user1):
     client.force_login(user1)
     resp = client.get(logout_url)
     assertRedirects(resp, '/')
+
+
+@pytest.mark.django_db
+@patch('users.views.GoogleAPIClient.get_oauth_authorization_url')
+def test_google_login_redirect(mock_get_oauth_authorization_url, client):
+    mock_get_oauth_authorization_url.return_value = '/oauth'
+    url = reverse('users:google-login-redirect')
+    response = client.get(url)
+    assertRedirects(response, '/oauth', fetch_redirect_response=False)
+
+
+@pytest.mark.django_db
+@patch('users.views.GoogleAPIClient.get_oauth_credentials')
+@patch('users.views.GoogleAPIClient.get_user_profile')
+def test_google_login_callback(
+    mock_get_user_profile, mock_get_oauth_credentials, client
+):
+    mock_get_oauth_credentials.return_value = {
+        'token': 'fake-token',
+        'refresh_token': 'fake-refresh-token',
+        'scopes': json.dumps(['scope1', 'scope2']),
+    }
+    mock_get_user_profile.return_value = {
+        'email': 'testuser@example.com',
+        'given_name': 'Test',
+        'family_name': 'User',
+    }
+
+    url = reverse('users:google-login-callback')
+    response = client.get(url, {'code': 'fake-code'})
+
+    assertRedirects(response, '/')
+    user = User.objects.get(email='testuser@example.com')
+    assert user.first_name == 'Test'
+    assert user.last_name == 'User'
+    profile = ExternalProfile.objects.get(user=user)
+    assert profile.google_access_token == 'fake-token'
+    assert profile.google_refresh_token == 'fake-refresh-token'
+    assert profile.google_scopes
+    google_scopes = json.loads(profile.google_scopes)
+    assert 'scope1' in google_scopes
+    assert 'scope2' in google_scopes
+    assert profile.google_enabled
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+
 from . import views
 
 app_name = 'users'
@@ -7,4 +8,10 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('login', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
+    path('oauth/google', views.google_login_redirect, name='google-login-redirect'),
+    path(
+        'oauth/google/callback',
+        views.google_login_callback,
+        name='google-login-callback',
+    ),
 ]
